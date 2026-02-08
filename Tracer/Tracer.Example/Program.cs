22e638b5ea7422a48405d41fc6f041b957e7b588
theexponenta <the.exponenta@gmail.com>
2026-02-09 02:44:23 +0300

    Implement Example (without threads yet)

---
+++ Tracer/Tracer.Example/Program.cs
@@ -0,0 +1,81 @@
+using System.Diagnostics;
+using Tracer.Core;
+using Tracer.Serialization;
+
+namespace Tracer.Example;
+
+public class Foo
+{
+    private Bar _bar;
+    private ITracer _tracer;
+
+    internal Foo(ITracer tracer)
+    {
+        _tracer = tracer;
+        _bar = new Bar(_tracer);
+    }
+    
+    public void MyMethod()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(100);
+        _bar.InnerMethod();
+        _tracer.StopTrace();
+    }
+}
+
+public class Bar
+{
+    private ITracer _tracer;
+
+    internal Bar(ITracer tracer)
+    {
+        _tracer = tracer;
+    }
+    
+    public void InnerMethod()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(200);
+        _tracer.StopTrace();
+    }
+}
+
+class Program
+{
+    private const string SOURCE_DIR_NAME = "Tracer.Example";
+
+    private static string GetSourceDirPath()
+    {
+        string baseDirectory = AppContext.BaseDirectory;
+        int sourceDirIndex = baseDirectory.LastIndexOf(SOURCE_DIR_NAME);
+        return baseDirectory.Substring(0, sourceDirIndex);
+    }
+
+    private static string GetSerializersPath()
+    {
+        return $"{GetSourceDirPath()}..{Path.DirectorySeparatorChar}Serializers";
+    }
+
+    private static string GetSerializationResultsDirPath()
+    {
+        return $"{GetSourceDirPath()}..{Path.DirectorySeparatorChar}SerializationResults";
+    }
+
+    static void Main(string[] args)
+    {        
+        MethodTracer tracer = new MethodTracer();
+        Foo foo = new Foo(tracer);
+        foo.MyMethod();
+
+        TraceResult result = tracer.GetTraceResult();
+
+        string serializersPath = GetSerializersPath();
+
+        var serializers = SerializationUtils.LoadSerializers(serializersPath);
+        foreach (var serializer in serializers)
+        {
+            SerializationUtils.SaveToFile(result, serializer, "result", GetSerializationResultsDirPath());
+        }
+    }
+}
